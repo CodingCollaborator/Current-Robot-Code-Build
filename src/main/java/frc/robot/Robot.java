6dd206e27b044d0ecccf3f46439b76a782ab419f
PiersonTeam28 <piersonrobotics@sagharborschools.org>
2025-03-09 10:53:09 -0400

    -m deleted more unused imports

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,16 +4,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.cscore.VideoCamera;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import org.opencv.core.Mat;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.imgproc.Imgproc;
-
 
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
